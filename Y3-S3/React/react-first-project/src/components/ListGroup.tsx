db0f20b945afb8f9da5de90d2735faa46b55639e
Blenstinwin2 <i516906@fhict.nl>
2024-09-20 22:38:35 +0200

    Used Type Annotation to specify the type of event handler; replaced and called it in line 16

--- Y3-S3/React/react-first-project/src/components/ListGroup.tsx
+++ Y3-S3/React/react-first-project/src/components/ListGroup.tsx
@@ -1,15 +1,5 @@
 function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
-
-  if (items.length === 0)
-    //in case there's no items, notify the user.
-    return (
-      <>
-        <h1>List</h1>
-        <p>No items found.</p>;
-      </>
-    );
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
   return (
     <>
@@ -18,7 +8,13 @@ function ListGroup() {
       {items.length === 0 && <p>No items found.</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li> //key is a unique identifier required by React - it gives errors in the browser if we don't include it
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(e) => console.log(e)}
+          >
+            {item}
+          </li> //key is a unique identifier required by React - it gives errors in the browser if we don't include it
         ))}
         {/* In JSX we can't use dynamic elements, hence we use {} to wrap that
         dynamic elements so we can use it. */}
