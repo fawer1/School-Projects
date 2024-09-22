f7b2d2cd592f5339bf305e9a43e41720c8d5fa6a
Blenstinwin2 <i516906@fhict.nl>
2024-09-22 20:20:53 +0200

    Added a button component with dynamic values using interface for its properties. Explored using default and optional values in the props

--- Y3-S3/React/react-first-project/src/App.tsx
+++ Y3-S3/React/react-first-project/src/App.tsx
@@ -1,10 +1,15 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 function App() {
   return (
     <div>
       <Alert>
         Hello <span>World</span>
       </Alert>
+      <Button color="secondary" onClick={() => console.log("Hello World")}>
+        Click Me
+      </Button>
+      <Button onClick={() => console.log("Hello World")}>Click Me</Button>
     </div>
   );
 }
---
+++ Y3-S3/React/react-first-project/src/components/Button.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  children: string;
+  color?: string; //? means optional
+  onClick: () => void;
+}
+const Button = ({ children, onClick, color = "primary" }: Props) => {
+  return (
+    <button className={"btn btn-" + color} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
