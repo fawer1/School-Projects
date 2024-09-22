26434e4fa86558838324338a20a092579f2356ff
Blenstinwin2 <i516906@fhict.nl>
2024-09-22 19:14:32 +0200

    Replaced the items array from ListGroup.tsx to App.tsx; Added an interface to type check the items and heading in the ListGroup function.

--- Y3-S3/React/react-first-project/src/components/ListGroup.tsx
+++ Y3-S3/React/react-first-project/src/components/ListGroup.tsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 
-function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+//Interface - define the structure and shape of an object --> type checking
+interface Props {
+  items: string[]; //array of strings
+  heading: string; //string
+}
+
+function ListGroup({ items, heading }: Props) {
   // Hook - when this funtion is called it rerenders the component to show the change.
   const [selectedIndex, setSelectedIndex] = useState(-1); // -1 means no item is selected
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {/*If the condition is true, return the paragraph.*/}
       {items.length === 0 && <p>No items found.</p>}
       <ul className="list-group">
@@ -18,13 +23,13 @@ function ListGroup() {
                 ? "list-group-item active"
                 : "list-group-item"
             }
-            key={item}
+            key={item} //key is a unique identifier required by React - it gives errors in the browser if we don't include it
             onClick={() => {
               setSelectedIndex(index);
             }}
           >
             {item}
-          </li> //key is a unique identifier required by React - it gives errors in the browser if we don't include it
+          </li>
         ))}
         {/* In JSX we can't use dynamic elements, hence we use {} to wrap that
         dynamic elements so we can use it. */}
